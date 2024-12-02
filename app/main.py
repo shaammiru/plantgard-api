308570ef3287fb39f4798c94fa5b858f1394b51f
shaammiru <msyamil.hamami@gmail.com>
2024-12-02 22:17:33 +0700

    :wrench: fix: update imports and add servers for FastAPI docs

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 
-from .routers import predicts, auths
+from routers import predicts, auths
 
-app = FastAPI()
-
-app.include_router(predicts.router)
-app.include_router(auths.router)
+app = FastAPI(
+    servers=[
+        {
+            "url": "http://localhost:8000",
+            "description": "Local Development Server",
+        }
+    ]
+)
 
 
 @app.get("/", tags=["Root"])
 async def root():
     return {"message": "Hello FastAPI!"}
+
+
+app.include_router(auths.router)
+app.include_router(predicts.router)
