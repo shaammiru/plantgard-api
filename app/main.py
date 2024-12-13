fb7bb2d4d01026718375b99a5e43ad5a9551ee2a
shaammiru <msyamil.hamami@gmail.com>
2024-12-13 08:13:44 +0700

    Add cors middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import os
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import predicts, auths
 
 load_dotenv()
@@ -18,6 +19,14 @@ app = FastAPI(
             "description": "Production Server",
         },
     ]
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
